f23e37141ea5795f60cd89b9d0bb85ad1e785474
André Timermann <andre@timermann.com.br>
2023-09-03 19:35:16 -0300

    remoção public e assets q são gerados no build

--- src/public/nfManager/main/_nuxt/index.db6371d6.js
+++
@@ -1,1 +0,0 @@
-import{c as e,o as a}from"./entry.33c58b1a.js";const n={__name:"index",setup(o){return(r,t)=>(a(),e("div",null," Dashboard "))}};export{n as default};
--- src/public/nfManager/main/_nuxt/logo.c04aa82d.js
+++
@@ -1,1 +0,0 @@
-import"./entry.33c58b1a.js";const t=""+new URL("logo.27295c69.png",import.meta.url).href;export{t as default};
