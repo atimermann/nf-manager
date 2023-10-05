914b7b0b3eabdc3c31fc7a01c456e71ec37953ef
André Timermann <andre@timermann.com.br>
2023-10-05 20:19:24 -0300

    nova tela listagem de job

---
+++ web/index.mjs
@@ -0,0 +1,15 @@
+/**
+ * **Created on 30/09/23**
+ *
+ * /index.mjs
+ * @author André Timermann <andre@timermann.com.br>
+ *
+ */
+
+import ErrorList from './src/components/job/errorList'
+import List from './src/components/job/List'
+
+export {
+  ErrorList as JobErrorList,
+  List as JobList
+}
